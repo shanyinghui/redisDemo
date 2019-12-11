0d9539901b6a9ebbdfeda20ca174a3beaccaba0e
shanyinghui <48811300@qq.com>
2019-12-11 18:19:43 +0800

    å¯添加å对key和value的序列化

--- src/main/java/com/buba/ridesdemo/RidesdemoApplication.java
+++ src/main/java/com/buba/ridesdemo/RidesdemoApplication.java
@@ -1,15 +1,11 @@
 package com.buba.ridesdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class RidesdemoApplication {
 
-    @Autowired
-    RedisTemplate redisTemplate;
     public static void main(String[] args) {
         SpringApplication.run(RidesdemoApplication.class, args);
     }
--- src/test/java/com/buba/ridesdemo/RidesdemoApplicationTests.java
+++ src/test/java/com/buba/ridesdemo/RidesdemoApplicationTests.java
@@ -1,10 +1,16 @@
 package com.buba.ridesdemo;
 
+import com.buba.ridesdemo.pojo.User;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootTest
 class RidesdemoApplicationTests {
@@ -13,10 +19,20 @@ class RidesdemoApplicationTests {
     RedisTemplate redisTemplate;
     @Test
     void contextLoads() {
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
         ValueOperations vo = redisTemplate.opsForValue();
-        vo.set("syh","123");
-        Object k1 = vo.get("syh");
-        System.out.println(k1);
+        User user = new User();
+        user.setId(1);
+        user.setName("张三");
+        user.setAge(23);
+        vo.set("user",user);
+        redisTemplate.boundValueOps("user1").set(user);
     }
 
 }
